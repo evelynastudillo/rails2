0f231ed26a49317fec38fb638e282fd37c547dd6
evelyn <astudillobarra.evelyn@gmail.com>
2018-09-06 20:05:24 -0300

    root commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'todos/index'
   get 'users/index'
-  root to: 'users#index'
+  root to: 'todos#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
