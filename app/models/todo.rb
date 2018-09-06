365b30523654e361697eaa64824316a8d6d60f14
evelyn <astudillobarra.evelyn@gmail.com>
2018-09-06 19:30:07 -0300

    first commit

---
+++ app/models/todo.rb
@@ -0,0 +1,5 @@
+class Todo < ApplicationRecord
+  has_many :completedtasks
+  has_many :users, through: :completedtasks
+
+end
---
+++ config/db/seeds.rb
@@ -0,0 +1,50 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(
+  name: 'Salir Temprano de la pega',
+  photo: "https://pbs.twimg.com/media/CHNqjisWkAAsUyG.png"
+)
+
+Todo.create(
+  name: 'Hacer Pebre',
+  photo: "http://www.midiariodecocina.com/wp-content/uploads/2016/07/Pebre-with-avocado01.jpg"
+)
+
+Todo.create(
+  name: 'Comer Asado',
+  photo: "http://www.theclinic.cl/wp-content/uploads/2012/09/asado-chileno.jpg"
+)
+
+Todo.create(
+  name: 'Comer Empanada',
+  photo: "http://assets.kraftfoods.com/recipe_images/opendeploy/76266_640x428.jpg"
+)
+
+
+Todo.create(
+  name: 'Ir a una Fonda',
+  photo: "https://cooperativa.cl/noticias/site/artic/20120914/imag/foto_0000000720120914133547.jpg"
+)
+Todo.create(
+  name: 'Tomar Terremotos (muchos)',
+  photo: "http://www.lasegunda.com/especiales/especial_18/img/terremoto.jpg"
+)
+Todo.create(
+  name: 'Bailar cueca (poco)',
+  photo: "http://cncmedios.cl/plus/wp-content/uploads/sites/3/2016/08/cueca.jpg"
+)
+
+Todo.create(
+  name: 'Encumbrar Volantines',
+  photo: "http://img.soy-chile.cl/Fotos/2014/08/28/file_20140828154251.jpg"
+)
+Todo.create(
+  name: 'Dieta Post 18',
+  photo: "http://d3f4nerl4dh38d.cloudfront.net/sites/elnaveghable.cl/files/imagecache/380x285/imagen_noticia/dieta_post_18.jpg"
+)
