bb0adeb8f64711f189bb70a35b76623d73c92bbf
evelyn <astudillobarra.evelyn@gmail.com>
2018-09-06 20:38:29 -0300

    commit completedtasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'todos/index'
-  get 'users/index'
+  resources :todos, only: :index do
+    resources :completedtasks, only: :create
+  end
+
   root to: 'todos#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
