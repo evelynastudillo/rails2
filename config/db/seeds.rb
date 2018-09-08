709b47dce6ea27918bb349f49d9885610737a1d2
evelyn <astudillobarra.evelyn@gmail.com>
2018-09-08 12:29:03 -0300

    commit

--- config/db/seeds.rb
+++ config/db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Todo.destroy_all
 
 Todo.create(
   name: 'Salir Temprano de la pega',
